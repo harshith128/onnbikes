c2f1197dab55815c18252bcb07ec9433a652787b
Pratik Jadhav <pratikjadhav718@gmail.com>
2021-08-27 14:08:44 +0530

    ride bar is now working

--- Pratik/all/pratik/script.js
+++ Pratik/all/pratik/script.js
@@ -1,3 +1,80 @@
+
+    function checkStartDate(){
+        let start_div = document.querySelector(".startDate");
+        let start_dateAt = start_div.querySelector("input").value;
+        let startYear = start_dateAt.substring(0, 4);
+        let startMonth = start_dateAt.substring(5, 7);
+        let startDate = start_dateAt.substring(8, 10);
+        let startHour = start_dateAt.substring(11, 13);
+        let startMin = start_dateAt.substring(14, 16);
+
+        let today = new Date();
+        let year = today.getFullYear();
+        let month = today.getMonth() + 1;
+        let date = today.getDate();
+        let hour = today.getHours();
+        let min = today.getMinutes();
+
+        if(startYear > year){
+            return true;
+        } else if(startYear == year && startMonth > month){
+            return true;
+        } else if(startYear == year && startMonth == month && startDate > date){
+            return true;
+        } else if(startYear == year && startMonth == month && startDate == date && startHour > hour){
+            return true;
+        } else if(startYear == year && startMonth == month && startDate == date && startHour == hour && startMin > min){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function callStart(){
+        if(!checkStartDate()){
+            alert("Please select Valid Starting Date and Time")
+        }
+    }
+    document.querySelector(".endDate").querySelector("input").addEventListener("click", callStart);
+
+    function checkEndDate(){
+        let start_div = document.querySelector(".startDate");
+        let start_dateAt = start_div.querySelector("input").value;
+        let startYear = start_dateAt.substring(0, 4);
+        let startMonth = start_dateAt.substring(5, 7);
+        let startDate = start_dateAt.substring(8, 10);
+        let startHour = start_dateAt.substring(11, 13);
+        let startMin = start_dateAt.substring(14, 16);
+
+        let end_div = document.querySelector(".endDate");
+        let end_dateAt = end_div.querySelector("input").value;
+        let endYear = end_dateAt.substring(0, 4);
+        let endMonth = end_dateAt.substring(5, 7);
+        let endDate = end_dateAt.substring(8, 10);
+        let endHour = end_dateAt.substring(11, 13);
+        let endMin = end_dateAt.substring(14, 16);
+
+        if(endYear > startYear){
+            return true;
+        } else if(endYear == startYear && endMonth > startMonth){
+            return true;
+        } else if(endYear == startYear && endMonth == startMonth && endDate > startDate){
+            return true;
+        } else if(endYear == startYear && endMonth == startMonth && endDate == startDate && endHour > startHour){
+            return true;
+        } else if(endYear == startYear && endMonth == startMonth && endDate == startDate && endHour == startHour && endMin > startMin){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+
+//
+
 function rideNow(){
         
     let select_city = document.querySelector(".city");
@@ -16,10 +93,17 @@ function rideNow(){
         alert("Please Select the start date");
     } else if(end_date === "") {
         alert("Please Select the end date")
+    } else if(!checkStartDate()){
+        alert("Please select Valid Start Date and Time"); 
+    } else if(!checkEndDate()){            
+        alert("Please select Valid End Date and Time");
     } else {
         localStorage.setItem("Date", JSON.stringify({city:city, plan:plan, startDate:start_date, endDate:end_date}));
     }
 }
+
+document.querySelector(".rideBtn").querySelector("button").addEventListener("click", rideNow);
+
 
 function rideNowAuto(){
     let date =JSON.parse(localStorage.getItem("Date"));
@@ -48,6 +132,8 @@ function rideNowAuto(){
     }
 }
 rideNowAuto();
+
+
 function filt(ckbox){
     var product = JSON.parse(localStorage.getItem("bike_data"))
     
