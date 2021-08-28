5a00564fa74be1142d48038cad12a2782ab65c12
Pratik Jadhav <pratikjadhav718@gmail.com>
2021-08-28 13:46:31 +0530

    most of the css part is done

--- Pratik/all/pratik/script.js
+++ Pratik/all/pratik/script.js
@@ -152,27 +152,79 @@ function filt(ckbox){
     newprod.forEach(function (products){
         
         var div = document.createElement('div');
+        div.classList.add("one_div1");
 
-        var name = document.createElement('P');
-        name.innerText = products.name;
+        var div_img_div = document.createElement('div');
+        div_img_div.classList.add("img_div");
 
-        var price = document.createElement("P");
-        price.innerText = products.price;
+        var img123 = document.createElement('img');
+        img123.classList.add("img123");
+        img123.src = products.image;
 
-        var image = document.createElement('img');
-        image.src = products.image;
+        div_img_div.append(img123)
+
+        var div_bk = document.createElement('div');
+
+        var div_bike_name = document.createElement('div');
+        div_bike_name.classList.add("bike_name_bk")
+        div_bike_name.innerText = products.name;
+
+        div_bk.append(div_bike_name);
+//div s_one
+        var div_s_one =document.createElement('div');
+        div_s_one.classList.add("s_one");
+        div_s_one.classList.add("flex-child");
+
+        var div_s_one_one =document.createElement('div');
+        div_s_one_one.classList.add("s_one_one");
+        div_s_one_one.innerText = "Free 904 kms"
+
+        var div_s_one_two =document.createElement('div');
+        div_s_one_two.classList.add("s_one_two");
+
+        var div_s_one_two_center =document.createElement('div');
+        div_s_one_two_center.classList.add("center11");
+        div_s_one_two_center.innerText = " ₹"+products.price;
+        
+        div_s_one_two.append( div_s_one_two_center);
+        div_s_one.append(div_s_one_one, div_s_one_two)
+
+        //for second part
+//div s_two
+        var div_s_two =document.createElement('div');
+        div_s_two.classList.add("s_two");
+        div_s_two.classList.add("flex-child");
+
+        var div_s_two_one =document.createElement('div');
+        div_s_two_one.classList.add("s_two_one");
+        div_s_two_one.innerText = "Exess ₹2/km"
+        
+        var div_s_two_two =document.createElement('div');
+        div_s_two_two.classList.add("s_two_two");
+
+        var div_s_two_two_booking =document.createElement('div');
+        div_s_two_two_booking.classList.add("s_two_booking");
 
         var btn = document.createElement('button');
+        btn.classList.add("btn11");
         btn.innerText = "Book Now";
 
         btn.onclick=(function (){
             bookNowClicked(products);
         })
 
-        div.append(image,name,price,btn);
+        div_s_two_two_booking.append(btn);
+        div_s_two_two.append(div_s_two_two_booking);
+        div_s_two.append(div_s_two_one,div_s_two_two);
+        
+        var onlyone = document.createElement('div');
+        onlyone.classList.add("flex-container");
+        onlyone.append(div_s_one,div_s_two)
+
+        div.append(div_img_div,div_bk,onlyone);
 
         data_div.append(div);
-    })      
+    }) 
 }
 
 function filt2(ckbox2){
@@ -193,24 +245,76 @@ function filt2(ckbox2){
     newprod.forEach(function (products){
         
         var div = document.createElement('div');
+        div.classList.add("one_div1");
 
-        var name = document.createElement('P');
-        name.innerText = products.name;
+        var div_img_div = document.createElement('div');
+        div_img_div.classList.add("img_div");
 
-        var price = document.createElement("P");
-        price.innerText = products.price;
+        var img123 = document.createElement('img');
+        img123.classList.add("img123");
+        img123.src = products.image;
 
-        var image = document.createElement('img');
-        image.src = products.image;
+        div_img_div.append(img123)
+
+        var div_bk = document.createElement('div');
+
+        var div_bike_name = document.createElement('div');
+        div_bike_name.classList.add("bike_name_bk")
+        div_bike_name.innerText = products.name;
+
+        div_bk.append(div_bike_name);
+//div s_one
+        var div_s_one =document.createElement('div');
+        div_s_one.classList.add("s_one");
+        div_s_one.classList.add("flex-child");
+
+        var div_s_one_one =document.createElement('div');
+        div_s_one_one.classList.add("s_one_one");
+        div_s_one_one.innerText = "Free 904 kms"
+
+        var div_s_one_two =document.createElement('div');
+        div_s_one_two.classList.add("s_one_two");
+
+        var div_s_one_two_center =document.createElement('div');
+        div_s_one_two_center.classList.add("center11");
+        div_s_one_two_center.innerText = " ₹"+products.price;
+        
+        div_s_one_two.append( div_s_one_two_center);
+        div_s_one.append(div_s_one_one, div_s_one_two)
+
+        //for second part
+//div s_two
+        var div_s_two =document.createElement('div');
+        div_s_two.classList.add("s_two");
+        div_s_two.classList.add("flex-child");
+
+        var div_s_two_one =document.createElement('div');
+        div_s_two_one.classList.add("s_two_one");
+        div_s_two_one.innerText = "Exess ₹2/km"
+        
+        var div_s_two_two =document.createElement('div');
+        div_s_two_two.classList.add("s_two_two");
+
+        var div_s_two_two_booking =document.createElement('div');
+        div_s_two_two_booking.classList.add("s_two_booking");
 
         var btn = document.createElement('button');
+        btn.classList.add("btn11");
         btn.innerText = "Book Now";
 
         btn.onclick=(function (){
             bookNowClicked(products);
         })
 
-        div.append(image,name,price,btn);
+        div_s_two_two_booking.append(btn);
+        div_s_two_two.append(div_s_two_two_booking);
+        div_s_two.append(div_s_two_one,div_s_two_two);
+        
+        var onlyone = document.createElement('div');
+        onlyone.classList.add("flex-container");
+        onlyone.append(div_s_one,div_s_two)
+
+        div.append(div_img_div,div_bk,onlyone);
 
         data_div.append(div);
     })
@@ -234,26 +338,76 @@ function filt3(ckbox3){
     newprod.forEach(function (products){
         
         var div = document.createElement('div');
-        div.setAttribute("class", "democlass");
+        div.classList.add("one_div1");
 
-        var name = document.createElement('P');
-        name.innerText = products.name;
+        var div_img_div = document.createElement('div');
+        div_img_div.classList.add("img_div");
 
-        var price = document.createElement("P");
-        price.innerText = products.price;
+        var img123 = document.createElement('img');
+        img123.classList.add("img123");
+        img123.src = products.image;
 
-        var image = document.createElement('img');
-        image.src = products.image;
+        div_img_div.append(img123)
+
+        var div_bk = document.createElement('div');
+
+        var div_bike_name = document.createElement('div');
+        div_bike_name.classList.add("bike_name_bk")
+        div_bike_name.innerText = products.name;
+
+        div_bk.append(div_bike_name);
+//div s_one
+        var div_s_one =document.createElement('div');
+        div_s_one.classList.add("s_one");
+        div_s_one.classList.add("flex-child");
+
+        var div_s_one_one =document.createElement('div');
+        div_s_one_one.classList.add("s_one_one");
+        div_s_one_one.innerText = "Free 904 kms"
+
+        var div_s_one_two =document.createElement('div');
+        div_s_one_two.classList.add("s_one_two");
+
+        var div_s_one_two_center =document.createElement('div');
+        div_s_one_two_center.classList.add("center11");
+        div_s_one_two_center.innerText = " ₹"+products.price;
+        
+        div_s_one_two.append( div_s_one_two_center);
+        div_s_one.append(div_s_one_one, div_s_one_two)
+
+        //for second part
+//div s_two
+        var div_s_two =document.createElement('div');
+        div_s_two.classList.add("s_two");
+        div_s_two.classList.add("flex-child");
+
+        var div_s_two_one =document.createElement('div');
+        div_s_two_one.classList.add("s_two_one");
+        div_s_two_one.innerText = "Exess ₹2/km"
+        
+        var div_s_two_two =document.createElement('div');
+        div_s_two_two.classList.add("s_two_two");
+
+        var div_s_two_two_booking =document.createElement('div');
+        div_s_two_two_booking.classList.add("s_two_booking");
 
         var btn = document.createElement('button');
+        btn.classList.add("btn11");
         btn.innerText = "Book Now";
 
         btn.onclick=(function (){
             bookNowClicked(products);
         })
-        
 
-        div.append(image,name,price,btn);
+        div_s_two_two_booking.append(btn);
+        div_s_two_two.append(div_s_two_two_booking);
+        div_s_two.append(div_s_two_one,div_s_two_two);
+        
+        var onlyone = document.createElement('div');
+        onlyone.classList.add("flex-container");
+        onlyone.append(div_s_one,div_s_two)
+
+        div.append(div_img_div,div_bk,onlyone);
 
         data_div.append(div);
     })
