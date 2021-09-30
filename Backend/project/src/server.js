d3c38f408dd8e0755321698b6a6ded20050a3ea8
Pratik Jadhav <pratikjadhav718@gmail.com>
2021-09-30 14:36:04 +0530

    cors

--- Backend/project/src/server.js
+++ Backend/project/src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const connect = require("./config/db.");
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 
